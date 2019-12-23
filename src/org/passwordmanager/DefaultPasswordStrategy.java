2a9a082bb5c70561e5027b819c110d4c0d95b092
kingchocomilk <gamereg911@gmail.com>
2019-12-23 12:41:18 -0800

    Implementing a way for a Strategy Pattern. There might be more ways to skin the cat.

--- src/org/passwordmanager/DefaultPasswordStrategy.java
+++ src/org/passwordmanager/DefaultPasswordStrategy.java
@@ -1,6 +1,17 @@
 package org.passwordmanager;
 
-public class DefaultPasswordStrategy implements PasswordStrategy {
+public class DefaultPasswordStrategy extends PasswordStrategy {
+
+    public DefaultPasswordStrategy(){
+        this.minimumLength = 6;
+        this.maximumLength = 12;
+        this.minimumLowerChars = 0;
+        this.minimumCapitalChars = 0;
+        this.minimumDigitChars = 0;
+        this.minimumSymbolChars = 0;
+        this.invalidCharacters = "";
+    }
+
     @Override
     public String makePassword() {
         return null;
--- src/org/passwordmanager/PasswordStrategy.java
+++ src/org/passwordmanager/PasswordStrategy.java
@@ -1,5 +1,22 @@
+/*
+    Author: Jose Eduardo Soto
+    Description:    The responsibility of this class is to encapsulate the algorithm of the password-building
+    strategy. It needs have all of the fields we need to generate.
+ */
+
 package org.passwordmanager;
 
-public interface PasswordStrategy {
-    String makePassword();
+public abstract class PasswordStrategy {
+    int minimumLength;
+    int maximumLength;
+    String invalidCharacters;
+    int minimumLowerChars;
+    int minimumCapitalChars;
+    int minimumDigitChars;
+    int minimumSymbolChars;
+
+    // The purpose of this is that it's the entry point of the algorithm for all of the classes that implement it.
+    // The problem with it is that there appears to be a problem sending data between what we generated and
+    // what we have. Maybe we can just make a data structure or something.
+    abstract String makePassword();
 }
